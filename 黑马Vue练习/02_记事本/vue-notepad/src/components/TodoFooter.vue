3ba3828c5465d29a9cdce9e748595017036ea5f2
lionsom <lionsom.linx@qq.com>
2023-08-15 16:57:56 +0800

    小黑记事本，组件化

---
+++ 黑马Vue练习/02_记事本/vue-notepad/src/components/TodoFooter.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="footerView">
+
+    <p> 总计：{{ MyCurrnetListNumber }} 条</p>
+
+    <button @click="clearAlllll">全部清除</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['MyCurrnetListNumber'],
+  methods: {
+    clearAlllll () {
+      console.log('clearAlllll')
+      this.$emit('clearAll', 'null')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.footerView {
+  display: flex;
+  justify-content: space-between;
+}
+
+p {
+  flex: 3;
+
+  text-align: left;
+  margin-left: 20px;
+}
+
+button {
+  flex: 1;
+
+  margin-right: 20px;
+}
+
+</style>
---
+++ 黑马Vue练习/02_记事本/vue-notepad/src/components/TodoHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    <h1>记事本</h1>
+
+    <input type="text" placeholder="请输入待办" @keyup.enter="handlerAddddd" v-model="todoName">
+
+    <button @click="handlerAddddd">添加任务</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      todoName: ''
+    }
+  },
+  methods: {
+    handlerAddddd () {
+      this.$emit('addd', this.todoName)
+      this.todoName = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
