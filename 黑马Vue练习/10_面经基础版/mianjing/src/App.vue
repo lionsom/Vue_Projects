3bd169cafd1bfd44b26b10548f2f58e429cbc508
lionsom <lionsom.linx@qq.com>
2023-08-16 19:30:43 +0800

    vue

---
+++ 黑马Vue练习/10_面经基础版/mianjing/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="h5-wrapper">
+    <div class="content">
+
+    </div>
+
+    <nav class="tabbar">
+      <a href="#/article">面经</a>
+      <a href="#/collect">收藏</a>
+      <a href="#/like">喜欢</a>
+      <a href="#/user">我的</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
+.h5-warpper {
+
+  margin: 0 auto;
+
+  width: 50%;
+  background: palegoldenrod;
+}
+
+.tabbar {
+  display: flex;
+  justify-content: space-around;
+
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  height: 60px;
+}
+
+.tabbar a {
+  flex: 1;
+  text-align: center;
+  line-height: 60px;
+
+  background: green;
+  border: 1px solid rebeccapurple;
+}
+
+</style>
---
+++ 黑马Vue练习/10_面经基础版/mianjing/src/views/Main_Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='h5-wrap'>
+    <div class="content">
+      <router-view></router-view>
+    </div>
+    <nav class='tabbar'>
+    <!-- 导航高亮
+      1. 将a标签，替换成router-link (to)
+      2. 结合高亮类名实现高亮效果 (router-link-active 模糊匹配)
+    -->
+    <router-link>首页</router-link>
+    <router-link>首页</router-link>
+    <router-link>首页</router-link>
+    <router-link>首页</router-link>
+  </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
