c098a81c6bfe69be5c579b35686e8726e7fd1f85
linxiang <linxiang@linxiangdeMBP.lan>
2023-08-17 00:58:23 +0800

    vue

--- 黑马Vue练习/10_面经基础版/mianjing/src/router/index.js
+++ 黑马Vue练习/10_面经基础版/mianjing/src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// View
+
+
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/',
+    component: 
+  }
 ]
 
 const router = new VueRouter({
