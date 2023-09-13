c511aadac7779e146b9948f9f08f7d3b05198eb2
lionsom <lionsom.linx@qq.com>
2023-09-13 19:32:52 +0800

    登录

--- all-in-one/src/router/index.js
+++ all-in-one/src/router/index.js
@@ -48,4 +48,28 @@ const router = new VueRouter({
   ]
 })
 
+// 所有的路由在真正被访问到之前(解析渲染对应组件页面前)，都会先经过全局前置守卫
+// 只有全局前置守卫放行了，才会到达对应的页面
+
+// 全局前置导航守卫
+// to:   到哪里去，到哪去的完整路由信息对象 (路径，参数)
+// from: 从哪里来，从哪来的完整路由信息对象 (路径，参数)
+// next(): 是否放行
+// (1) next()     直接放行，放行到to要去的路径
+// (2) next(路径)  进行拦截，拦截到next里面配置的路径
+
+// 定义一个数组，专门用户存放所有需要权限访问的页面
+const authUrls = ['/shopping-pay', '/shopping-myorder']
+
+router.beforeEach((to, from, next) => {
+  console.log(to, from, next)
+
+  if (authUrls.includes(to.path)) {
+    // 权限页面】
+    //
+  }
+  // 直接放行
+  next()
+})
+
 export default router
