f717fcae6ff1440536036b1338ae897c1c90cb97
lionsom <lionsom.linx@qq.com>
2023-11-07 19:33:50 +0800

    ts

---
+++ all-in-one/src/views/ts-usage/11/2.ts
@@ -0,0 +1,14 @@
+// function getFirstWord(message: string) {
+//   console.log(message.split(' ')[0])
+// }
+
+// getFirstWord('Hello World') // 输出 Hello
+
+// getFirstWord(123) // TypeError: msg.split is not a function
+
+function greeter(person: string) {
+  return 'Hello, ' + person
+}
+
+let user = [0, 1, 2]
+console.log(user)
---
+++ all-in-one/src/views/ts-usage/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="main">TypeScript Demo</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
