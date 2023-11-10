db8a7ec53137c3ad481f78591e22f61625b045b8
lionsom <lionsom.linx@qq.com>
2023-11-10 17:50:17 +0800

    vue3

---
+++ vue3-demo/src/views/demo-views/11-defineOptions-before.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  setup () {
+    // 数据
+    const message = 'hello Vue3'
+    // 函数
+    const logMessage = () => {
+      console.log(message)
+    }
+    return {
+      message,
+      logMessage
+    }
+  },
+
+  name: 'setup-base-page',
+
+  beforeCreate () {
+    console.log('beforeCreate函数')
+  }
+}
+</script>
+
---
+++ vue3-demo/src/views/demo-views/11-defineOptions-before2.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'test-index-111'
+}
+</script>
+
+<script setup>
+const message = 'this is a message'
+const logMessage = () => {
+  console.log(message)
+}
+</script>
+
+<template>
+  <div>{{ message }}</div>
+  <button @click="logMessage">按钮</button>
+</template>
