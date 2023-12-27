749fdcba3dc023ea823241dc5259071d717a95c8
linxiang <lionsom.linx@qq.com>
2023-12-27 18:37:50 +0800

    webpack

--- 《Vue3 入门指南与实战案例》/07_Hello_TypeScript/src/ts/index.ts
+++ 《Vue3 入门指南与实战案例》/07_Hello_TypeScript/src/ts/index.ts
@@ -5,7 +5,7 @@ console.log(md5('Hello World'))
 
 
 function getFirstWord(msg: string) {
-    console.log(msg.split(' ')[0])
-  }
+  console.log(msg.split(' ')[0])
+}
   
-  getFirstWord('Hello World')
+getFirstWord('Hello World')
