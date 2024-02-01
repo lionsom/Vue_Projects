df13fda752c41c2aae44d4972b7103ba1e87eea9
linxiang <lionsom.linx@qq.com>
2024-02-01 14:01:28 +0800

    gitignore

---
+++ 06-Webpack/mydemo/02-webpack打包样式文件/webpack.config.js
@@ -0,0 +1,13 @@
+/*
+  webpack.config.js  webpack的配置文件
+    作用: 指示 webpack 干哪些活（当你运行 webpack 指令时，会加载里面的配置）
+
+    所有构建工具都是基于nodejs平台运行的~模块化默认采用commonjs。
+*/
+
+module.exports = {
+  // webpack配置
+  // 入口起点
+  entry: './src/index.js',
+
+}
