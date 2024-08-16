2d7d128b4483f8feb9deb8d7810210a8e162caab
linxiang <lionsom.linx@qq.com>
2024-08-16 15:53:58 +0800

    整理

--- typescript-base/src/compile/index.ts
+++
@@ -1,9 +0,0 @@
-import greet from './greet'
-
-// 单个问候语
-const greeting = greet('Petter')
-console.log(greeting)
-
-// 多个问候语
-const greetings = greet(['Petter', 'Tom', 'Jimmy'])
-console.log(greetings)
--- typescript-base/src/package/index.ts
+++
@@ -1,6 +0,0 @@
-// 需要安装两个包
-// pnpm add -S md5 
-// pnpm add -D @types/md5  
-
-import md5 from 'md5'
-console.log(md5('Hello World'))
