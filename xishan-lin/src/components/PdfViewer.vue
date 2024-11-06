adfd9b1ac2f109bb10d86137379a3f01ece8e684
linxiang <lionsom.linx@qq.com>
2024-11-06 17:28:30 +0800

    vue

---
+++ xishan-lin/src/components/PdfViewer.vue
@@ -0,0 +1,30 @@
+
+<script lang="ts">
+export default {
+  name: 'PdfViewer'
+}
+</script>
+
+<script setup lang="ts">
+import pdfjs from "pdfjs-dist";
+
+import * as PDFJS from 'pdfjs-dist/legacy/build/pdf.mjs'
+import * as PdfWorker from 'pdfjs-dist/legacy/build/pdf.worker.mjs'
+
+import { ref, onMounted, watch } from 'vue';
+
+const props = defineProps({
+  
+})
+
+
+</script>
+
+<template>
+  <div ref="pdfContainer" class="pdf-container"></div>
+</template>
+
+
+<style scoped lang="scss">
+
+</style>
